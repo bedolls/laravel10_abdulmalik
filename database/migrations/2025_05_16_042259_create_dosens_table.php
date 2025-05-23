6e30007bc7b6181efc84e279afda4d976447dda9
A <147518138+bedolls@users.noreply.github.com>
2025-05-23 10:50:51 +0700

    Update 2025_05_16_042259_create_dosens_table.php

--- database/migrations/2025_05_16_042259_create_dosens_table.php
+++ database/migrations/2025_05_16_042259_create_dosens_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('mahasiswas', function (Blueprint $table) {
+        Schema::create('dosens', function (Blueprint $table) {
             $table->id();
             $table->string('nidn')->unique();
             $table->string('nama','100');
